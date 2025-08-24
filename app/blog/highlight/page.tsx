4837561d52b1e3568553dfdecdb3bedfaddc7c3b
hothaifa zoubi <hothaifazoubi123@gmail.com>
2025-08-24 11:16:41 +0300

    fdsgdsfg ds

---
+++ app/blog/highlight/page.tsx
@@ -0,0 +1,10 @@
+export default function BlogPage() {
+    return (
+        <>
+        <div>
+            <h1>This is the Highlight page</h1>
+            <p>BABA - Lirone want to sleep</p>
+        </div>
+        </>
+    )
+}
---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+export default function BlogPage() {
+    return (
+        <>
+        <div>
+            <h1>This is the blog page</h1>
+            <p>BABA - OMEr Come BAKc i need vacation</p>
+        </div>
+        </>
+    )
+}
